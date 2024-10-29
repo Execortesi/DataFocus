52ef7ecafbf66f603953f188eb7d4c85396734b3
Execortesi <execonerc@gmail.com>
2024-10-29 09:08:28 -0300

    Agrego documentacion de funcion

--- R/descarga-lee-Datos.R
+++ R/descarga-lee-Datos.R
@@ -1,4 +1,21 @@
-# Descarga y lee los datos de estaciones
+#' Descarga y lee datos de estaciones
+#'
+#' Esta función descarga datos de una estación meteorlóguca especifica y los lee en un data frame.
+#'
+#' @param id_estacion Un identificador de estación meteorologica específica. Debe ser uno de los siguientes: "NH0472", "NH0910", "NH0046", "NH0098", "NH0437".
+#' @param ruta_archivo Ruta de destino donde se guardara el archivo descargado.
+#'
+#'
+#' @return
+#' Un data frame que contiene los datos de estación meteorlógica seleccionada.
+#'
+#'
+#' @examples
+#' descargar_leer_datos_estacion("NH0472", "data/NH0472.csv")
+#'
+#'
+#' @export
+
 descargar_leer_datos_estacion <- function(id_estacion, ruta_archivo) {
   estaciones_urls <- list(
     NH0472 = "https://raw.githubusercontent.com/rse-r/intro-programacion/main/datos/NH0472.csv",
@@ -18,7 +35,7 @@ descargar_leer_datos_estacion <- function(id_estacion, ruta_archivo) {
 }
 
 # Ejemplo de uso
-# datos <- descargar_leer_datos_estacion("NH0472", "R/NH0472.csv")
+# datos <- descargar_leer_datos_estacion("NH0472", "data/NH0472.csv")
 
 
 
--- R/dias_lluviosos.R
+++ R/dias_lluviosos.R
@@ -1,7 +1,1 @@
 #Funcion que detcta los dias de lluvia intensa
-detectar_dias_lluvia_intensa <- function(datos, umbral) {
-  dias_lluviosos <- subset(datos, precipitacion_pluviometrica > umbral)
-  return(dias_lluviosos)
-}
-
-detectar_dias_lluvia_intensa(datos,20)
