a0e368c0c430df1fb40c01779d995e5fbe597df6
Bruno991-rgb <brunocarrara1027@gmail.com>
2024-10-29 12:08:33 -0300

    agrego cambios

--- R/tabla_resumen_temperatura.R
+++ R/tabla_resumen_temperatura.R
@@ -1,15 +1,23 @@
-# Función que genera una tabla de resumen de la temperatura_abrigo_150cm para las estaciones
-
-tabla_resumen_temperatura <- function(datos) {
-  resumen <- datos %>%
-    group_by(id) %>%
-    summarise(
-      min_temp = min(temperatura_abrigo_150cm, na.rm = TRUE),
-      max_temp = max(temperatura_abrigo_150cm, na.rm = TRUE),
-      mean_temp = mean(temperatura_abrigo_150cm, na.rm = TRUE)
-    )
-  return(resumen)
-}
+#' Tabla resumen
+#'
+#' Esta funcion devuelve una tabla resumen de la temperatura abrigo
+#'
+#' @param datos
+#'
+#' @return
+#' (resumen)
+#'
+#' @examples
+#'tabla_resumen_temperatura <- function(datos) {
+#'  resumen <- datos %>%
+#'    group_by(id) %>%
+#'    summarise(
+#'      min_temp = min(temperatura_abrigo_150cm, na.rm = TRUE),
+#'      max_temp = max(temperatura_abrigo_150cm, na.rm = TRUE),
+#'      mean_temp = mean(temperatura_abrigo_150cm, na.rm = TRUE)
+#'    )
+#'  return(resumen)
+#'}
 
 
 
